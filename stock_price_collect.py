9e4b8467bb4df51e155c1457402bd843c1d51dc9
dribblewithclong <dribble.clong@gmail.com>
2023-03-18 15:03:02 +0700

    add script for collecting stock price

---
+++ stock_price_collect.py
@@ -0,0 +1,34 @@
+"""
+daily: to_time = from_time + 1 day
+"""
+
+from pandas_datareader import data as pdr
+import yfinance as yf
+
+
+class StockPriceCollector:
+
+    def __init__(self, stock_symbol, from_time, to_time):
+        self.from_time = from_time
+        self.to_time = to_time
+        self.stock_symbol = stock_symbol
+
+        yf.pdr_override()
+
+    def get_price(self):
+        price = pdr.get_data_yahoo(
+            self.stock_symbol,
+            self.from_time,
+            self.to_time,
+        )['Adj Close'].to_dict()
+
+        return price
+
+    def get_volume(self):
+        volume = pdr.get_data_yahoo(
+            self.stock_symbol,
+            self.from_time,
+            self.to_time
+        )['Volume'].to_dict()
+
+        return volume
